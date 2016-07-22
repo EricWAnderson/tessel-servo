a272e4f11ba27dd02a35764003d31f0cf67097fa
EricWAnderson <eric429@gmail.com>
2016-07-22 00:20:26 -0500

    feed a certain amount

--- servo/servo.js
+++ servo/servo.js
@@ -15,22 +15,25 @@ var servo = servolib.use(tessel.port['A']);
 
 var servo1 = 1; // We have a servo plugged in at position 1
 
-servo.on('ready', function () {
-  var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
+var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
+var amount = 10;
 
+servo.on('ready', function () {
   //  Set the minimum and maximum duty cycle for servo 1.
   //  If the servo doesn't move to its full extent or stalls out
   //  and gets hot, try tuning these values (0.05 and 0.12).
   //  Moving them towards each other = less movement range
   //  Moving them apart = more range, more likely to stall and burn out
   servo.configure(servo1, 0.05, 0.12, function () {
-    feedCat(position);
+    feedCat(amount);
   });
 });
 
-function feedCat(position){
-  setInterval(function () {
+function feedCat(amount){
+  var counter = 0;
+  var timer = setInterval(function () {
     console.log('Position (in range 0-1):', position);
+    console.log(counter);
 
     //  Set servo #1 to position pos.
     servo.move(servo1, position);
@@ -39,6 +42,13 @@ function feedCat(position){
     position += 0.1;
     if (position > 1) {
       position = 0; // Reset servo position
+      counter++;
+    }
+
+    if (counter >= amount) {
+      console.log('hit limit');
+      clearInterval(timer);
+      return;
     }
 
   }, 50); // Every 500 milliseconds
